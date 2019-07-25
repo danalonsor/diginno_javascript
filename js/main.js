168bee8089350cb0ba0f8e8cede1b991ee398106
danalonsor <danalonsor@gmail.com>
2019-07-24 21:01:13 -0300

    Criado index add esquema add pasta js e main

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var nome = "Daniel Alonso";
+var idade = 37;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+
+//alert("Meu nome é" + nome + "tenho" + idade + "anos");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+/*console.log(frase.toUpperCase());
+console.log(frase.toLowerCase()); */
